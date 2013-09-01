3de8b7486018cd606868f91ab4f0dd8b5e1f2d85
Vladislav Bezverkhiy <rabbiabram@gmail.com>
2013-09-01 21:22:45 +0700

    Some changes

--- script.rb
+++ script.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'redis'
+require 'shotgun'
+require 'rubygems'
+require 'lorem-ipsum'
+
+not_found do
+  status 404
+  "Something wrong"
+end
 
 get '/' do
   "Hello World!"
+  erb :index, :layout => :'layouts/default'
+end
+
+
+get '/lorem' do
+  @gen = LoremIpsum::Generator.new
+  @gen.analyze 'lorem.txt'
+  erb :index, :layout => :'layouts/default'
 end
